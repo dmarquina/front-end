09f33d2195c70f8a936820af1511578b4da6d68e
Diego Marquina Rojas <mrdiego0892@gmail.com>
2019-05-16 02:11:44 -0500

    new dist

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -2,6 +2,6 @@ export class Constants {
     constructor() {
     }
 
-    public static IP_API_CONNECTION: string = 'http://54.234.246.18:8080';
+    public static IP_API_CONNECTION: string = 'http://172-31-92-210:8080';
     public static CONTENT_TYPE_APP_JSON: string = 'application/stream+json';
   }
